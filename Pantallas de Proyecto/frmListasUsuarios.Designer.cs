f065b9fb612811da3a07d04118ea399e65cc2138
ALG-99 <alma199902@yahoo.com>
2021-11-10 21:29:03 -0600

    Eliminacion de formulario de agregar usuario
    Se elimino formulario y se agrego la informacion a un solo lista de usuarios

--- Pantallas de Proyecto/frmListasUsuarios.Designer.cs
+++ Pantallas de Proyecto/frmListasUsuarios.Designer.cs
@@ -37,6 +37,19 @@ namespace Pantallas_de_Proyecto
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNombreGestor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbNivelUser = new System.Windows.Forms.ComboBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtCodEmpleado = new System.Windows.Forms.TextBox();
+            this.estado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListaUsuarios)).BeginInit();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -45,22 +58,22 @@ namespace Pantallas_de_Proyecto
             // dgvListaUsuarios
             // 
             this.dgvListaUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListaUsuarios.Location = new System.Drawing.Point(202, 95);
-            this.dgvListaUsuarios.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvListaUsuarios.Location = new System.Drawing.Point(344, 343);
+            this.dgvListaUsuarios.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvListaUsuarios.Name = "dgvListaUsuarios";
             this.dgvListaUsuarios.RowHeadersWidth = 51;
             this.dgvListaUsuarios.RowTemplate.Height = 24;
-            this.dgvListaUsuarios.Size = new System.Drawing.Size(662, 434);
+            this.dgvListaUsuarios.Size = new System.Drawing.Size(735, 368);
             this.dgvListaUsuarios.TabIndex = 0;
             this.dgvListaUsuarios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListaUsuarios_CellContentClick);
             // 
             // btnAgregar
             // 
             this.btnAgregar.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnAgregar.Location = new System.Drawing.Point(38, 153);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Location = new System.Drawing.Point(51, 188);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(110, 84);
+            this.btnAgregar.Size = new System.Drawing.Size(147, 103);
             this.btnAgregar.TabIndex = 1;
             this.btnAgregar.Text = "Agregar Usuario";
             this.btnAgregar.UseVisualStyleBackColor = false;
@@ -69,10 +82,10 @@ namespace Pantallas_de_Proyecto
             // btnModificar
             // 
             this.btnModificar.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnModificar.Location = new System.Drawing.Point(38, 256);
-            this.btnModificar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModificar.Location = new System.Drawing.Point(51, 315);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(110, 84);
+            this.btnModificar.Size = new System.Drawing.Size(147, 103);
             this.btnModificar.TabIndex = 2;
             this.btnModificar.Text = "Modificar Usuario";
             this.btnModificar.UseVisualStyleBackColor = false;
@@ -86,21 +99,21 @@ namespace Pantallas_de_Proyecto
             this.groupBox3.Controls.Add(this.btnCancelar);
             this.groupBox3.Controls.Add(this.btnModificar);
             this.groupBox3.Controls.Add(this.btnAgregar);
-            this.groupBox3.Location = new System.Drawing.Point(2, 2);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Location = new System.Drawing.Point(3, 2);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox3.Size = new System.Drawing.Size(196, 722);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(261, 889);
             this.groupBox3.TabIndex = 121;
             this.groupBox3.TabStop = false;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(38, 370);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(51, 455);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 84);
+            this.button1.Size = new System.Drawing.Size(147, 103);
             this.button1.TabIndex = 5;
             this.button1.Text = "Mostar Usuarios";
             this.button1.UseVisualStyleBackColor = false;
@@ -108,10 +121,10 @@ namespace Pantallas_de_Proyecto
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Pantallas_de_Proyecto.Properties.Resources.logo_3__2_;
-            this.pictureBox1.Location = new System.Drawing.Point(20, 17);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 21);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 117);
+            this.pictureBox1.Size = new System.Drawing.Size(211, 144);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -119,10 +132,10 @@ namespace Pantallas_de_Proyecto
             // btnCancelar
             // 
             this.btnCancelar.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnCancelar.Location = new System.Drawing.Point(34, 492);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelar.Location = new System.Drawing.Point(45, 606);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(112, 84);
+            this.btnCancelar.Size = new System.Drawing.Size(149, 103);
             this.btnCancelar.TabIndex = 4;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -132,23 +145,150 @@ namespace Pantallas_de_Proyecto
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Wide Latin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(211, 19);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(281, 23);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(232, 18);
+            this.label6.Size = new System.Drawing.Size(279, 21);
             this.label6.TabIndex = 122;
             this.label6.Text = "Lista de Usuarios";
             // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(545, 220);
+            this.txtEstado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(472, 22);
+            this.txtEstado.TabIndex = 138;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(389, 291);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 137;
+            // 
+            // txtNombreGestor
+            // 
+            this.txtNombreGestor.Location = new System.Drawing.Point(545, 130);
+            this.txtNombreGestor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombreGestor.Name = "txtNombreGestor";
+            this.txtNombreGestor.Size = new System.Drawing.Size(472, 22);
+            this.txtNombreGestor.TabIndex = 136;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 135;
+            this.label1.Text = "Contraseña:";
+            // 
+            // cmbNivelUser
+            // 
+            this.cmbNivelUser.FormattingEnabled = true;
+            this.cmbNivelUser.Items.AddRange(new object[] {
+            "admin",
+            "superv",
+            "gestor"});
+            this.cmbNivelUser.Location = new System.Drawing.Point(545, 173);
+            this.cmbNivelUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbNivelUser.Name = "cmbNivelUser";
+            this.cmbNivelUser.Size = new System.Drawing.Size(472, 24);
+            this.cmbNivelUser.TabIndex = 134;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(545, 269);
+            this.txtContraseña.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(184, 22);
+            this.txtContraseña.TabIndex = 133;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(649, 83);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(168, 22);
+            this.txtUsuario.TabIndex = 132;
+            // 
+            // txtCodEmpleado
+            // 
+            this.txtCodEmpleado.Location = new System.Drawing.Point(450, 83);
+            this.txtCodEmpleado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCodEmpleado.Name = "txtCodEmpleado";
+            this.txtCodEmpleado.Size = new System.Drawing.Size(100, 22);
+            this.txtCodEmpleado.TabIndex = 131;
+            // 
+            // estado
+            // 
+            this.estado.AutoSize = true;
+            this.estado.Location = new System.Drawing.Point(393, 220);
+            this.estado.Name = "estado";
+            this.estado.Size = new System.Drawing.Size(52, 17);
+            this.estado.TabIndex = 130;
+            this.estado.Text = "Estado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 17);
+            this.label4.TabIndex = 129;
+            this.label4.Text = "Nombre del Gestor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(390, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "Nivel Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(571, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 127;
+            this.label2.Text = "Usuario:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 126;
+            this.label5.Text = "Codigo:";
+            // 
             // frmListasUsuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(885, 857);
+            this.ClientSize = new System.Drawing.Size(1180, 1055);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNombreGestor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbNivelUser);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtCodEmpleado);
+            this.Controls.Add(this.estado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.dgvListaUsuarios);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmListasUsuarios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lista de Usuarios ";
@@ -171,5 +311,18 @@ namespace Pantallas_de_Proyecto
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNombreGestor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbNivelUser;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtCodEmpleado;
+        private System.Windows.Forms.Label estado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
     }
 }
